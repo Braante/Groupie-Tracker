356558da6b717517a6c43221250070f29a9596c1
VBRAVO <valentin.bravo@ynov.com>
2022-01-28 13:59:21 +0100

    valentin

--- serv/server.go
+++ serv/server.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"groupie"
-	"net/http"
-	"text/template"
+    "fmt"
+    "groupie"
+    "net/http"
 )
 
 type ToDoPage struct {
-	PageTitle string
+    PageTitle string
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("static/index.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		groupie.MainHandler(w, r)
-		data := ToDoPage{}
-		tmpl.Execute(w, data)
-	})
+    //tmpl := template.Must(template.ParseFiles("static/index.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r http.Request) {
+        groupie.MainHandler(w, r)
+        /data := ToDoPage{}
+        tmpl.Execute(w, data)*/
+    })
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+    http.HandleFunc("/second", func(w http.ResponseWriter, r http.Request) {
+        groupie.MainHandlersec(w, r)
+        /data := ToDoPage{}
+        tmpl.Execute(w, data)*/
+    })
 
-	err := http.ListenAndServe("localhost:8080", nil)
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	if err != nil {
-		fmt.Println(err)
-	}
+    err := http.ListenAndServe("localhost:8080", nil)
+
+    if err != nil {
+        fmt.Println(err)
+    }
 }
